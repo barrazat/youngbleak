ee59666ba8039806887c5cd6b0df94d1df4b1521
Leonardo Barraza <leobarrazat@gmail.com>
2021-08-24 22:01:15 -0400

    Version 0.1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Young Bleak
+Proyecto web para artista
+
+El objetivo de la página es simplemente otorgar información a los usuarios que la visiten.
---
+++ js/main.js
@@ -0,0 +1,19 @@
+let menu = document.getElementById("menu")
+let header = document.getElementById("header")
+let nav = document.getElementById("nav")
+menu.addEventListener("click", function(){
+    if(header.style.height == "50px" || header.offsetHeight == 50) {
+        header.style.height = 50 + nav.offsetHeight + "px";
+    } else {
+        header.style.height = "50px"
+    }
+    
+})
+
+window.addEventListener("resize", function(){
+    let ancho = document.documentElement.clientWidth
+
+    if(ancho > 480) {
+        header.style = ""
+    }
+})
