17593b7859e163a146c30581abc0887e21001540
Leonardo Barraza <leobarrazat@gmail.com>
2021-08-30 18:28:52 -0400

    Implementa mejora en barra de navegacion y otros

--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
 let menu = document.getElementById("menu")
 let header = document.getElementById("header")
 let nav = document.getElementById("nav")
+
+function ajustarTamañoHeader(){
+    header.style.height = "50px"
+}
+
 menu.addEventListener("click", function(){
     if(header.style.height == "50px" || header.offsetHeight == 50) {
         header.style.height = "100%";
     } else {
-        header.style.height = "50px"
+        ajustarTamañoHeader();
     }
     
 })
+
+document.getElementById("home").onclick = function(){
+    if(header.style.height == "100%"){
+        ajustarTamañoHeader();
+    }
+}
+
+document.getElementById("bio").onclick = function(){
+    if(header.style.height == "100%"){
+        ajustarTamañoHeader();
+    }
+}
+
+document.getElementById("references").onclick = function(){
+    if(header.style.height == "100%"){
+        ajustarTamañoHeader();
+    }
+}
+
+document.getElementById("prices").onclick = function(){
+    if(header.style.height == "100%"){
+        ajustarTamañoHeader();
+    }
+}
 
 window.addEventListener("resize", function(){
     let ancho = document.documentElement.clientWidth
